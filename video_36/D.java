c230bda692e45f266146a5aaf9dcb66ae1bd7c76
krishankansal <63016271+krishankansal@users.noreply.github.com>
2023-06-19 09:16:46 +0530

    Add files via upload

---
+++ video_36/D.java
@@ -0,0 +1,10 @@
+class D{
+
+    public static void main(String[] args) {
+        System.out.println("a"+10+10); // a1010
+        System.out.println(10+10+"a"); // 20a
+        System.out.println(10+10+"a"+10+10); // 20a1010
+        System.out.println(10+10+"a"+10+10+"a"); //20a1010a
+    }
+}
+
