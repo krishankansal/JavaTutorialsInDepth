7f50039b363ea766624db17331387d2067b0d2c2
krishankansal <63016271+krishankansal@users.noreply.github.com>
2023-06-23 14:27:32 +0530

    Add files via upload

---
+++ video_42/D.java
@@ -0,0 +1,11 @@
+// Cretae subclass
+public class D extends C {
+
+    int k = 40;
+    int j = 300; // variable hiding
+
+    void sum() {
+        System.out.println("sum = " + (i + j + k));
+    }
+
+}
---
+++ video_42/gen/A.java
@@ -0,0 +1,12 @@
+package gen;
+
+import art.Figure;;
+
+public class A extends Figure {
+    public static void main(String[] args) {
+        A f = new A();
+        f.resize();
+    }
+
+}
+
