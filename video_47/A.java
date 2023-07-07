0ebe4b695452419d004e4ae0a9ec626ec6ad37d4
krishankansal <63016271+krishankansal@users.noreply.github.com>
2023-07-07 07:37:29 +0530

    Add files via upload

---
+++ video_47/A.java
@@ -0,0 +1,20 @@
+public final class A {
+
+    final int YES = 1;
+    final int NO = 2;
+
+    final void meth() {
+        System.out.println("hello");
+    }
+
+    void bye() {
+        System.out.println("bye");
+    }
+
+    public static void main(String[] args) {
+        A ob = new A();
+        System.out.println(ob.YES);
+
+    }
+
+}
---
+++ video_47/B.java
@@ -0,0 +1,9 @@
+public class B extends A {
+
+    void bye() {
+        System.out.println("finally bye");
+    }
+
+    
+
+}
